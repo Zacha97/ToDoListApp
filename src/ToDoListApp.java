e4a19b20f89bd77e1a6b8b0800d75209542e5852
Zacha <grzegorz.zachariasz1997@gmail.com>
2023-07-13 00:22:34 +0200

    methods added: addTask, editTask, deleteTask, displayTask

--- src/ToDoListApp.java
+++ src/ToDoListApp.java
@@ -1,6 +1,35 @@
+import java.util.ArrayList;
 public class ToDoListApp {
+    private ArrayList<Task> tasks;
 
-    public static void main(String[] args) {
+    public ToDoListApp() {
+        tasks = new ArrayList<>();
+    }
 
+    public void addTask(Task task) {
+    tasks.add(task);
+    }
+
+    public void editTask (Task taskToEdit){
+        int index = tasks.indexOf(taskToEdit);
+        if (index >= 0) {
+            Task task = tasks.get(index);
+        }
+    }
+
+    public void deleteTask(Task taskToDelete){
+        int index = tasks.indexOf(taskToDelete);
+        if( index >= 0){
+            tasks.remove(index);
+        }
+    }
+
+    public void displayTask(){
+        for (Task task : tasks) {
+            System.out.println("Task's name: " + task.getName());
+            System.out.println("Task's ID: " + task.getId());
+            System.out.println("Task's description: " + task.getDescription());
+            System.out.println("Task's creation date: " + task.getCreation_date());
+        }
     }
 }
